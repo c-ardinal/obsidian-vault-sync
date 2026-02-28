1e6cde4f6274dd36949406fe2f015147231bd3e4
c-ardinal <0rdinal.inf0tec.e1ectr0nics@gmail.com>
2026-03-01 00:18:42 +0900

    feat: Add Codecov settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
     test: {
@@ -12,4 +13,12 @@ export default defineConfig({
             obsidian: "./tests/__mocks__/obsidian.ts",
         },
     },
+    plugins: [
+        // Put the Codecov vite plugin after all other plugins
+        codecovVitePlugin({
+            enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+            bundleName: "obsidian-vault-sync",
+            uploadToken: process.env.CODECOV_TOKEN,
+        }),
+    ],
 });
